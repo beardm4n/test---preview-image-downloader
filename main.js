c88a0ae26c260b0a9d45c2645d59e8c8d2201774
beardm4n <dmitriykorovko@gmail.com>
2019-11-05 17:15:05 +0300

    updated preview

--- main.js
+++ main.js
@@ -1,25 +1,23 @@
-const $input = document.querySelector('input[type="file"]');
+const $input = document.querySelector('input[type="file"]'),
+      $output = document.querySelector('#list'),
+      $img = document.createElement('img');
 
 $input.addEventListener('change', handleFileSelect);
 
 function handleFileSelect(e) {
    let file = e.target.files[0];
-   console.log(file.name);
+
    if (!file.type.match('image.*')) {
       alert('You must select a picture');
    }
-
-
-   const $img = document.createElement('img'),
-         $div = document.createElement('div');
 
    const reader = new FileReader();
 
    reader.onload = e => {
       $img.src = e.target.result;
       $img.classList.add('image');
-      document.querySelector('#list').insertBefore($div, null);
-      document.querySelector('div').insertBefore($img, null);
+
+      $output.insertBefore($img, null);
    };
 
    reader.readAsDataURL(file);
