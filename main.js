f9ddff387c054c086bd749c29bb1867060a8ce65
beardm4n <dmitriykorovko@gmail.com>
2019-11-05 21:15:20 +0300

    added some fixes

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 const $input = document.querySelector('input[type="file"]'),
-      $output = document.querySelector('#list'),
-      $img = document.createElement('img');
+    $output = document.querySelector('#list'),
+    $div = document.createElement('div'),
+    $img = document.createElement('img'),
+    $label = document.createElement('label'),
+    $inputRange = document.createElement('input');
+
+$div.setAttribute('class', 'range');
+$label.setAttribute('for', 'inputRange');
+$inputRange.setAttribute('id', 'inputRange');
+$inputRange.setAttribute('type', 'range');
+
+$input.innerHTML = 'Drag image';
+$label.innerHTML = 'Resize image';
 
 $input.addEventListener('change', handleFileSelect);
 
@@ -17,6 +28,10 @@ function handleFileSelect(e) {
       $img.src = e.target.result;
       $img.classList.add('image');
 
+      document.querySelector('section').insertBefore($div, $output);
+
+      $div.insertBefore($label, null);
+      $div.insertBefore($inputRange, null);
       $output.insertBefore($img, null);
    };
 
