67081136992701e83beb46d70064e75745674ec3
beardm4n <dmitriykorovko@gmail.com>
2019-11-06 16:27:19 +0300

    added an event handler for resizing the image

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
 const $input = document.querySelector('input[type="file"]'),
-    $output = document.querySelector('#list'),
-    $div = document.createElement('div'),
-    $img = document.createElement('img'),
-    $label = document.createElement('label'),
-    $inputRange = document.createElement('input');
+      $output = document.querySelector('#list'),
+      $div = document.createElement('div'),
+      $img = document.createElement('img'),
+      $label = document.createElement('label'),
+      $inputRange = document.createElement('input'),
+      $span = document.createElement('span');
+
 
 $div.setAttribute('class', 'range');
 $label.setAttribute('for', 'inputRange');
 $inputRange.setAttribute('id', 'inputRange');
 $inputRange.setAttribute('type', 'range');
+$inputRange.setAttribute('min', '0');
+$inputRange.setAttribute('max', '30');
+$inputRange.setAttribute('value', '15');
 
 $input.innerHTML = 'Drag image';
 $label.innerHTML = 'Resize image';
 
-$input.addEventListener('change', handleFileSelect);
+const averageSizeImage = $inputRange.getAttribute('value');
 
-function handleFileSelect(e) {
+$input.addEventListener('change', (e) => {
    let file = e.target.files[0];
 
    if (!file.type.match('image.*')) {
@@ -27,6 +32,8 @@ function handleFileSelect(e) {
    reader.onload = e => {
       $img.src = e.target.result;
       $img.classList.add('image');
+      $img.style.width = averageSizeImage + '%';
+      $img.style.height = averageSizeImage + '%';
 
       document.querySelector('section').insertBefore($div, $output);
 
@@ -36,4 +43,23 @@ function handleFileSelect(e) {
    };
 
    reader.readAsDataURL(file);
-}
+});
+
+$inputRange.addEventListener('click', () => {
+   setTimeout(() => {
+
+      console.log($inputRange.value);
+      $img.style.width = $inputRange.value + '%';
+      $img.style.height = $inputRange.value + '%';
+   }, 1000);
+
+   if ($inputRange.value > averageSizeImage) {
+      $span.innerHTML = `Increased by ${$inputRange.value - averageSizeImage}%`;
+   } else if ($inputRange.value < averageSizeImage) {
+      $span.innerHTML = `Decreased by ${averageSizeImage - $inputRange.value}%`;
+   } else {
+      $span.innerHTML = ``;
+   }
+
+   $output.insertBefore($span, $img)
+});
